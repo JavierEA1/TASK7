b7e1e5efc95fdba39d9a1900ea40fe90959e661b
JavierEA1 <jesteban97@gmail.com>
2021-03-03 15:31:00 +0100

    fINAL

--- TASK7/app.R
+++ TASK7/app.R
@@ -6,15 +6,22 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+library(tidyverse)
 library(shiny)
+list_choices=list("Histogram Age","Histogram Mileage")
+list_choices2=list("Plot Age","Plot Mileage")
+
+library(Stat2Data)
+data(AccordPrice)
+Data=AccordPrice
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
+ui <- fluidPage(navbarPage("Shiny app",
+                           tabPanel("Histogram",
+    fluidPage(                              
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
+    titlePanel("AccordPrice  Data"),
+    
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
@@ -22,27 +29,48 @@ ui <- fluidPage(
                         "Number of bins:",
                         min = 1,
                         max = 50,
-                        value = 40)
-        ),
-
+                        value = 20),
+        
+            selectInput("select", label = h3("Plot by type of data"), 
+                        choices = list_choices,
+                        selected = 1)  
+            ),
+        
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot")
-        )
+                        )
+                    )
+                )
+            )
+        ,
+    tabPanel("Plot",
+             fluidPage(
+                 sidebarLayout(sidebarPanel(
+                     selectInput("select2", label = h3("Plots"), 
+                                 choices = list_choices2,
+                                 selected = 1)
+                 ), mainPanel(
+                     h3("Plots"),
+                     plotOutput(outputId = "hello")
+                 )
+                 )))
+    
     )
 )
-
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
+        x    <- Data$Age
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
+    
+ 
+    
 }
 
 # Run the application 
